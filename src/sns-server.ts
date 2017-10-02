bc0f6bad4af07604f3395989888c8bd7589480c0
Matthew James <matthew.stephen.james@gmail.com>
2017-10-02 12:13:11 +1100

    event format

--- src/sns-server.ts
+++ src/sns-server.ts
@@ -1,9 +1,9 @@
-import {SNS} from "aws-sdk";
-import {Topic, TopicsList, Subscription, ListSubscriptionsResponse, CreateTopicResponse} from "aws-sdk/clients/sns.d";
+import { SNS } from "aws-sdk";
+import { Topic, TopicsList, Subscription, ListSubscriptionsResponse, CreateTopicResponse } from "aws-sdk/clients/sns.d";
 import fetch from "node-fetch";
 import { IDebug, ISNSServer } from "./types";
 import bodyParser = require("body-parser");
-import {v4 as uuid} from "uuid";
+import { v4 as uuid } from "uuid";
 import * as xml from "xml";
 const createAttr = () => {
     return {
@@ -59,14 +59,14 @@ export class SNSServer implements ISNSServer {
             this.debug(JSON.stringify(req.body));
             this.debug(JSON.stringify(this.subscriptions));
             if (req.body.Action === "ListSubscriptions") {
-                this.debug("sending: " + xml(this.listSubscriptions(), {indent: "\t"}));
+                this.debug("sending: " + xml(this.listSubscriptions(), { indent: "\t" }));
                 res.send(xml(this.listSubscriptions()));
             } else if (req.body.Action === "CreateTopic") {
                 res.send(xml(this.createTopic(req.body.Name)));
             } else if (req.body.Action === "Subscribe") {
                 res.send(xml(this.subscribe(req.body.Endpoint, req.body.Protocol, req.body.TopicArn)));
             } else if (req.body.Action === "Publish") {
-                res.send(xml(this.publish(req.body.TopicArn, req.body.Message, req.body.MessageStructure)));
+                res.send(xml(this.publish(req.body.TopicArn, req.body.subject, req.body.Message, req.body.MessageStructure)));
             } else if (req.body.Action === "Unsubscribe") {
                 res.send(xml(this.unsubscribe(req.body.SubscriptionArn)));
             } else {
@@ -166,17 +166,44 @@ export class SNSServer implements ISNSServer {
         };
     }
 
-    public publish(topicArn, message, messageType) {
+    public createEvent(topicArn, subscriptionArn, subject, message) {
+        return {
+            Records: [
+                {
+                    EventVersion: "1.0",
+                    EventSubscriptionArn: subscriptionArn,
+                    EventSource: "aws:sns",
+                    Sns: {
+                        SignatureVersion: "1",
+                        Timestamp: new Date().toISOString(),
+                        Signature: "EXAMPLE",
+                        SigningCertUrl: "EXAMPLE",
+                        MessageId: uuid(),
+                        Message: message,
+                        MessageAttributes: {},
+                        Type: "Notification",
+                        UnsubscribeUrl: "EXAMPLE",
+                        TopicArn: topicArn,
+                        Subject: subject,
+                    },
+                },
+            ],
+        };
+    }
+
+    public publish(topicArn, subject, message, messageType) {
         Promise.all(this.subscriptions.filter(sub => sub.TopicArn === topicArn).map(sub => {
             this.debug("fetching: " + sub.Endpoint);
+            const event = JSON.stringify(this.createEvent(topicArn, sub.SubscriptionArn, subject, message));
+            this.debug("event: " + event);
             return fetch(sub.Endpoint, {
                 method: "POST",
-                body: message,
+                body: event,
                 headers: {
                     "Content-Type": "application/json",
-                    "Content-Length": message.length,
-                } })
-                .then(res => this.debug(res));
+                    "Content-Length": event.length,
+                },
+            }).then(res => this.debug(res));
         }));
         return {
             PublishResponse: [
